a824c945c6f7c84c9cf9df8e4aa358b7936522b0
funny <funnychen@vectorbuilder.net>
2026-06-15 11:04:50 +0800

    #update

--- src/components/app-table/AppTable.jsx
+++ src/components/app-table/AppTable.jsx
@@ -1,10 +1,11 @@
 import {Empty, Table} from "antd";
 
 function AppTable(props) {
+    const { data, columns } = props
     return (
         <>
-            {props.data.length > 0 && <Table columns={props.columns} dataSource={props.data}></Table>}
-            {props.data.length === 0 && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>}
+            {data.length > 0 && <Table columns={columns} dataSource={data}></Table>}
+            {data.length === 0 && <Empty image={Empty.PRESENTED_IMAGE_SIMPLE}/>}
         </>
     )
 }
